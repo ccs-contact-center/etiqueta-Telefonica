c3d4f1df74ab6ae6f01a1e6522629fd15e49c773
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-05 12:24:08 -0500

    Update TemaTresView.js

--- src/views/temarioViews/TemaTresView.js
+++ src/views/temarioViews/TemaTresView.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react'
 import { Card, CardBody, CardHeader, Col, Row, Button } from 'reactstrap'
-import temaTresView from '../../assets/img/img-cursoQA/temaTresView.jpg'
-import Modal1 from './componentsExtras/modales'
-import {
-  TablaATC,
-  TablaVentas,
-  TablasVentasOut,
-} from './componentsExtras/tablas'
-import Nota from './componentsExtras/notasReferencia'
+import elemetos from '../../assets/img/ImgEtiquetaTelefonica/elemetos.png'
 class TemaDosView extends Component {
   loading = () => (
     <div className="animated fadeIn pt-1 text-center">Cargando...</div>
@@ -16,31 +9,36 @@ class TemaDosView extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-      
-          <CardHeader className="text-left">
+        <CardHeader className="text-left">
           <h3>Elementos de una etiqueta telefonica</h3>
-          </CardHeader>
-          <CardBody>
-            <Row>
-              <Col xs="12" className="centrado-fila">
-                <img
-                  src={temaTresView}
-                  style={{ width: 360 }}
-                  alt="temaTresView"
-                  className="img-fluid bordeImagen"
-                />
-              </Col>
-              <Col xs="12" className="text-justify">
-               
-              </Col>
-              <Col xs="12" className="centrado-distribuido">
-               
-                
-              </Col>
-            
-            </Row>
-          </CardBody>
-  
+        </CardHeader>
+        <CardBody>
+          <Row>
+            <Col xs="12" className="centrado-fila">
+              <img
+                src={elemetos}
+                style={{ width: 300 }}
+                alt="elemetos"
+                className="img-fluid"
+              />
+            </Col>
+            <Col xs="12" className="centrado-fila mb-2">
+              <div className="cajaA1" style={{width:'250px'}}>
+                <p className="text-center" style={{marginBottom:'0px'}}><b>ENTONACIÓN</b></p>
+              </div>
+            </Col>
+            <Col xs="12" className="centrado-fila mb-2">
+              <div className="cajaA1" style={{width:'250px'}}>
+                <p className="text-center" style={{marginBottom:'0px'}}><b>ARTICULACIÓN</b></p>
+              </div>
+            </Col>
+            <Col xs="12" className="centrado-fila">
+              <div className="cajaA1" style={{width:'250px'}}>
+                <p className="text-center" style={{marginBottom:'0px'}}><b>LOCUCIÓN O RITMO</b> </p>
+              </div>
+            </Col>
+          </Row>
+        </CardBody>
       </div>
     )
   }
