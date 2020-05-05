2b9c63ec50e6f2c3ef09267eaffceca79c4a8685
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-05 13:25:14 -0500

    Update ActividadView.js

--- src/views/temarioViews/ActividadView.js
+++ src/views/temarioViews/ActividadView.js
@@ -1,40 +1,183 @@
-import React, { Component } from 'react';
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  Col,
-  Row
-} from 'reactstrap';
-
-import List2 from './componentsExtras/dragdrog';
+import React, { Component } from 'react'
+import { CardBody, CardHeader, Col, Row, Button, Form, FormGroup, Label, Input, FormText  } from 'reactstrap'
 
 class ActividadView extends Component {
+  loading = () => (
+    <div className="animated fadeIn pt-1 text-center">Cargando...</div>
+  )
 
+  constructor(props) {
+    super(props)
+    this.state = { value: '' }
 
-  
-  loading = () => <div className="animated fadeIn pt-1 text-center">Cargando...</div>
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value)
+    event.preventDefault()
+  }
 
   render() {
-
     return (
       <div className="animated fadeIn">
-
         <Row>
           <Col>
-            
-              <CardHeader className="">
-               <h2> Actividad</h2>
-              </CardHeader>
-              <CardBody className="text-center">
-             <List2></List2>
-              </CardBody>
-          
+            <CardHeader className="">
+              <h2> Actividad</h2>
+            </CardHeader>
+            <CardBody className="text-center">
+              <form onSubmit={this.handleSubmit}>
+                <label>
+                  Name:
+                  <input
+                    className=""
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                  />
+                </label>
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Submit"
+                />
+              </form>
+
+              <Form>
+                <FormGroup row>
+                  <Label for="exampleEmail" sm={2}>
+                    Email
+                  </Label>
+                  <Col sm={10}>
+                    <Input
+                      type="email"
+                      name="email"
+                      id="exampleEmail"
+                      placeholder="with a placeholder"
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label for="examplePassword" sm={2}>
+                    Password
+                  </Label>
+                  <Col sm={10}>
+                    <Input
+                      type="password"
+                      name="password"
+                      id="examplePassword"
+                      placeholder="password placeholder"
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label for="exampleSelect" sm={2}>
+                    Select
+                  </Label>
+                  <Col sm={10}>
+                    <Input type="select" name="select" id="exampleSelect">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </Input>
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label for="exampleSelectMulti" sm={2}>
+                    Select Multiple
+                  </Label>
+                  <Col sm={10}>
+                    <Input
+                      type="select"
+                      name="selectMulti"
+                      id="exampleSelectMulti"
+                      multiple
+                    >
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </Input>
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label for="exampleText" sm={2}>
+                    Text Area
+                  </Label>
+                  <Col sm={10}>
+                    <Input type="textarea" name="text" id="exampleText" />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label for="exampleFile" sm={2}>
+                    File
+                  </Label>
+                  <Col sm={10}>
+                    <Input type="file" name="file" id="exampleFile" />
+                    <FormText color="muted">
+                      This is some placeholder block-level help text for the
+                      above input. It's a bit lighter and easily wraps to a new
+                      line.
+                    </FormText>
+                  </Col>
+                </FormGroup>
+                <FormGroup tag="fieldset" row>
+                  <legend className="col-form-label col-sm-2">
+                    Radio Buttons
+                  </legend>
+                  <Col sm={10}>
+                    <FormGroup check>
+                      <Label check>
+                        <Input type="radio" name="radio2" /> Option one is this
+                        and that—be sure to include why it's great
+                      </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                      <Label check>
+                        <Input type="radio" name="radio2" /> Option two can be
+                        something else and selecting it will deselect option one
+                      </Label>
+                    </FormGroup>
+                    <FormGroup check disabled>
+                      <Label check>
+                        <Input type="radio" name="radio2" disabled /> Option
+                        three is disabled
+                      </Label>
+                    </FormGroup>
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label for="checkbox2" sm={2}>
+                    Checkbox
+                  </Label>
+                  <Col sm={{ size: 10 }}>
+                    <FormGroup check>
+                      <Label check>
+                        <Input type="checkbox" id="checkbox2" /> Check me out
+                      </Label>
+                    </FormGroup>
+                  </Col>
+                </FormGroup>
+                <FormGroup check row>
+                  <Col sm={{ size: 10, offset: 2 }}>
+                    <Button>Submit</Button>
+                  </Col>
+                </FormGroup>
+              </Form>
+            </CardBody>
           </Col>
         </Row>
       </div>
-    );
+    )
   }
 }
 export default ActividadView
-
