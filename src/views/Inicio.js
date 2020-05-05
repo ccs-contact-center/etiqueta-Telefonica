bde494d94f08404f173fb7bec4e4ed4383b7e4e9
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-05 12:24:44 -0500

    Update Inicio.js

--- src/views/Inicio.js
+++ src/views/Inicio.js
@@ -39,8 +39,8 @@ class Inicio extends Component {
       <div className="animated fadeIn">
         <Card>
           {this.state.index === 0 ? (
-           <TemaTresView />
-            //<BienvenidaView />
+          
+            <BienvenidaView />
           ) : this.state.index === 1 ? (
             <PresentacionView />
           ) : this.state.index === 2 ? (
