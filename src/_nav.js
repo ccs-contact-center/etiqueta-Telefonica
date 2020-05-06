7d78820694aeb8da2f496f7c05b231ec0b7c1997
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-06 10:45:21 -0500

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -6,38 +6,162 @@ export default {
       class: 'text-center',
     },
     {
-      name: 'Etiqueta Telefonica',
-      icon: 'icon-home',
+      name: 'ETIQUETA TELEFÓNICA',
+      icon: 'icon-tag',
       children: [
         {
-          name: 'Objetivo',
-         // url: '/temarioViews/BienvenidaView',
-         icon: 'icon-directions',
+          name: 'ETIQUETA TELEFÓNICA',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+          children: [
+            {
+              name: '¿QUÉ ES ETIQUETA TELEFONICA?',
+              // url: '/temarioViews/BienvenidaView',
+              icon: 'icon-directions',
+            },
+            {
+              name: 'IMPORTANCIA DE LA ETIQUETA TELEFÓNICA ',
+              // url: '/temarioViews/BienvenidaView',
+              icon: 'icon-directions',
+            },
+            {
+              name: 'ALGUNAS REGLAS ',
+              // url: '/temarioViews/BienvenidaView',
+              icon: 'icon-directions',
+            },
+          ],
         },
-        {
-          name: 'Importancia de la Etiqueta Telefónica',
-         // url: '/temarioViews/ObjetivoView',
-         icon: 'icon-graduation',
-        },
-        {
-          name: 'Elementos de la Etiqueta Telefónica',
-         // url: '/temarioViews/PresentacionView',
-          icon: 'icon-graduation',
-        },
-      
-    
-        {
-          name: 'Evaluación',
-         // url: '/temarioViews/ActividadView',
-          icon: 'icon-puzzle'}
-          
-        
-       
-        
       ],
-        
-        
     },
-         
+
+    {
+      name: 'IMAGEN CORPORATIVA',
+      icon: 'icon-tag',
+      children: [
+        {
+          name: 'DE QUE SE ENCARGA EL AGENTE TELEFÓNICO',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+          children: [
+            {
+              name: 'RESPONSABILIDAD',
+              // url: '/temarioViews/BienvenidaView',
+              icon: 'icon-directions',
+            },
+            {
+              name: 'HERRAMIENTA DE APOYO ',
+              // url: '/temarioViews/BienvenidaView',
+              icon: 'icon-directions',
+            },
+          ],
+        },
+      ],
+    },
+
+    {
+      name: 'ESCUCHA ACTIVA',
+      icon: 'icon-tag',
+      children: [
+        {
+          name: 'COMUNICACIÓN TELEFÓNICA',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+        {
+          name: 'INTERFERENCIAS',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+      ],
+    },
+
+    {
+      name: 'FRASES ETIQUETA TELEFÓNICA',
+      icon: 'icon-tag',
+      children: [
+        {
+          name: 'CORTESÍA',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+        {
+          name: 'ESCUCHA ACTIVA',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+        {
+          name: 'GENERAR EMPATÍA',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+        {
+          name: 'SOLICITAR INFORMACIÓN',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+      ],
+    },
+
+    {
+      name: 'GESTIÓN DE SILENCIOS',
+      icon: 'icon-tag',
+      children: [
+        {
+          name: 'VISIÓN',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+        {
+          name: 'MANEJO',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+      ],
+    },
+
+    {
+      name: 'EMPATÍA',
+      icon: 'icon-tag',
+      children: [
+        {
+          name: 'ACTITUDES EMPÁTICAS',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+        {
+          name: 'ACTITUDES NO EMPÁTICAS',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+      ],
+    },
+
+    {
+      name: 'RESOLUCIÓN DE PROBLEMAS',
+      icon: 'icon-tag',
+      children: [
+        {
+          name: 'TIEMPOS DE CONVERSACIÓN',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+        {
+          name: 'RESOLUCIÓN DE INCIDENCIAS',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+        {
+          name: 'CLIENTE SATISFECHO',
+          // url: '/temarioViews/BienvenidaView',
+          icon: 'icon-directions',
+        },
+      ],
+    },
+
+    {
+      name: 'EVALUACIÓN',
+      icon: 'icon-puzzle',
+    
+    },
   ],
-};
+}
