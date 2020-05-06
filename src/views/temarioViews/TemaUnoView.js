3eeb13cbeb653c2c07c232cc67e5b7ca77e01d04
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-06 02:22:50 -0500

    Update TemaUnoView.js

--- src/views/temarioViews/TemaUnoView.js
+++ src/views/temarioViews/TemaUnoView.js
@@ -11,11 +11,11 @@ class TemaUnoView extends Component {
     return (
       <div className="animated fadeIn">
         <CardHeader className="text-left">
-          <h3> Importancia de la Etiqueta Telefónica</h3>
+          <h3> ETIQUETA TELEFÓNICA</h3>
         </CardHeader>
-        <CardBody className="text-center">
+        <CardBody className="">
           <Row>
-            <Col xs="12">
+            <Col xs="12" className="text-center">
               <img
                 src={actitudPositiva}
                 style={{ width: 250}}
@@ -23,8 +23,17 @@ class TemaUnoView extends Component {
                 className="img-fluid bordeImagen animated slideInUp "
               />
             </Col>
+            <Col xs="12 mt-3">
+              <h1 className="animated slideInUp"><b>¿Qué es etiqueta telefónica?</b></h1>
+              <h2 className="animated slideInUp">Manera “asertiva“ de tratar al cliente. </h2>
+            </Col>
             <Col xs="12">
-              <h1 className="animated slideInUp"><b>¡Actitud positiva!</b></h1>
+              <h1 className="animated slideInUp"><b>IMPORTANCIA DE LA ETIQUETA TELEFÓNICA </b></h1>
+              <h2 className="animated slideInUp">La forma en que se contesta el teléfono dice mucho de la empresa y también de la persona.</h2>
+            </Col>
+            <Col xs="12">
+              <h1 className="animated slideInUp"><b>ALGUNAS REGLAS</b></h1>
+              <h2 className="animated slideInUp"><b> Imagen corporativa, Escucha activa, Gestión de silencios, Empatía, Resolución de problemas.</b></h2>
             </Col>
           </Row>
         </CardBody>
