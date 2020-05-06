6e501db55e6c4cb36cf7353898a742dc95205798
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-06 02:22:22 -0500

    Update ActividadView.js

--- src/views/temarioViews/ActividadView.js
+++ src/views/temarioViews/ActividadView.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react'
-import { CardBody, CardHeader, Col, Row, Button, Form, FormGroup, Label, Input, FormText  } from 'reactstrap'
+import {
+  CardBody,
+  CardHeader,
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Table,
+} from 'reactstrap'
 
 class ActividadView extends Component {
   loading = () => (
@@ -29,51 +40,149 @@ class ActividadView extends Component {
         <Row>
           <Col>
             <CardHeader className="">
-              <h2>  Test de autoevaluación</h2>
+              <h2> Test de autoevaluación</h2>
             </CardHeader>
-            <CardBody className="text-center">
-             
-
+            <CardBody className="">
               <Form>
-                <FormGroup tag="fieldset" row>
-                  <legend className="col-form-label col-sm-2">
-                    Radio Buttons
-                  </legend>
-                  <Col sm={10}>
-                    <FormGroup check>
-                      <Label check>
-                        <Input type="radio" name="radio2" /> Option one is this
-                        and that—be sure to include why it's great
-                      </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                      <Label check>
-                        <Input type="radio" name="radio2" /> Option two can be
-                        something else and selecting it will deselect option one
-                      </Label>
-                    </FormGroup>
-                    <FormGroup check disabled>
-                      <Label check>
-                        <Input type="radio" name="radio2" disabled /> Option
-                        three is disabled
-                      </Label>
-                    </FormGroup>
-                  </Col>
-                </FormGroup>
-                <FormGroup row>
-                  <Label for="checkbox2" sm={2}>
-                    Checkbox
-                  </Label>
-                  <Col sm={{ size: 10 }}>
-                    <FormGroup check>
-                      <Label check>
-                        <Input type="checkbox" id="checkbox2" /> Check me out
-                      </Label>
-                    </FormGroup>
-                  </Col>
-                </FormGroup>
+                <div className="cajaA2" style={{ backgroundColor: '#d5d4d8' }}>
+                  <FormGroup tag="fieldset" row>
+                    <legend className="col-form-label ">
+                      ¿Qué es etiqueta telefónica?{' '}
+                      <span style={{ color: 'red' }}>*</span>
+                    </legend>
+                    <Col sm={12}>
+                      <FormGroup check>
+                        <Input type="radio" name="radio1" />
+                        <Label check>
+                          Manera "asertiva" de tratar al cliente.{' '}
+                        </Label>
+                      </FormGroup>
+                      <FormGroup check>
+                        <Input type="radio" name="radio2" />
+                        <Label check>Como hacer la llamada más ágil. </Label>
+                      </FormGroup>
+                      <FormGroup check>
+                        <Input type="radio" name="radio3" />
+                        <Label check>
+                          La estructura de tratar una llamada.{' '}
+                        </Label>
+                      </FormGroup>
+                    </Col>
+                  </FormGroup>
+                </div>
+
+                <div
+                  className="cajaA2 mt-3"
+                  style={{ backgroundColor: '#d5d4d8' }}
+                >
+                  <FormGroup tag="fieldset" row>
+                    <legend className="col-form-label ">
+                      La imagen corporativa se trata de:{' '}
+                      <span style={{ color: 'red' }}>*</span>
+                    </legend>
+                    <Col sm={12}>
+                      <FormGroup check>
+                        <Input type="radio" name="radio4" />
+                        <Label check>
+                          Crear un ambiente agradable para el usuario, dejando
+                          una buena imagen de la empresa.
+                        </Label>
+                      </FormGroup>
+                      <FormGroup check>
+                        <Input type="radio" name="radio5" />
+                        <Label check>
+                          Es recordar a cada momento de la llamada a la empresa
+                          que se comunicó.{' '}
+                        </Label>
+                      </FormGroup>
+                      <FormGroup check>
+                        <Input type="radio" name="radio6" />
+                        <Label check>
+                          Es hablarle al cliente de la empresa y decirle que se
+                          comunicó al mejor lugar para atenderle.
+                        </Label>
+                      </FormGroup>
+                    </Col>
+                  </FormGroup>
+                </div>
+
+                <div
+                  className="cajaA2 mt-3 text-center"
+                  style={{ backgroundColor: '#d5d4d8' }}
+                >
+                  <FormGroup tag="fieldset" row>
+                    <legend className="col-form-label ">
+                      Con base a tu conocimiento relaciona las filas con las
+                      columnas. El tema es "Escucha Activa"{' '}
+                      <span style={{ color: 'red' }}>*</span>
+                    </legend>
+                    <Col sm={12}>
+                      <Table className="table-border ">
+                        <tr>
+                          <td>
+                            <Label check>Emisor</Label>
+                            <FormGroup check>
+                              <Input type="radio" name="radio2" />
+                            </FormGroup>
+                          </td>
+                          <td>
+                            <Label check>
+                              Escuchar y entender la comunicación desde el punto
+                              de vista del que habla (cliente).
+                            </Label>
+                            <FormGroup check>
+                              <Input type="radio" name="radio2" />
+                            </FormGroup>
+                          </td>
+                          <td>
+                            <Label check>Canal</Label>
+                            <FormGroup check>
+                              <Input type="radio" name="radio2" />
+                            </FormGroup>
+                          </td>
+                          <td>
+                            <Label check>Receptor</Label>
+                            <FormGroup check>
+                              <Input type="radio" name="radio2" />
+                            </FormGroup>
+                          </td>
+                          <td>
+                            {' '}
+                            <Label check>Mensaje</Label>{' '}
+                            <FormGroup check>
+                              <Input type="radio" name="radio2" />
+                            </FormGroup>
+                          </td>
+                          <td>
+                            <Label check>Solución Problema</Label>
+                            <FormGroup check>
+                              <Input type="radio" name="radio2" />
+                            </FormGroup>
+                          </td>
+                          <td>
+                            <Label check>Escucha del problema</Label>
+                            <FormGroup check>
+                              <Input type="radio" name="radio2" />
+                            </FormGroup>
+                          </td>
+                          <td>
+                            <Label check>
+                              Entender la solución desde el punto de vista del
+                              que habla (cliente). Comunicación telefónica.
+                              Interferencias
+                            </Label>
+                            <FormGroup check>
+                              <Input type="radio" name="radio2" />
+                            </FormGroup>
+                          </td>
+                        </tr>
+                      </Table>
+                    </Col>
+                  </FormGroup>
+                </div>
+
                 <FormGroup check row>
-                  <Col sm={{ size: 10, offset: 2 }}>
+                  <Col sm={{ size: 10 }} className="centrado-fila">
                     <Button>Submit</Button>
                   </Col>
                 </FormGroup>
