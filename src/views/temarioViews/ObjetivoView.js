fe85e35b60d7b43f9e6486d13325183eb1a79777
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-11 10:29:29 -0500

    Update ObjetivoView.js

--- src/views/temarioViews/ObjetivoView.js
+++ src/views/temarioViews/ObjetivoView.js
@@ -12,32 +12,33 @@ class ObjetivoView extends Component {
     return (
       <div className="animated fadeIn">
         <CardHeader className="text-left">
-          <h3>Resolucón de Problemas</h3>
+          <h3>Objetivo</h3>
         </CardHeader>
         <CardBody className="">
           <Row className="centrado-fila">
-      
-              <Col xs="12" sm="12" md="5" lg="5">
-                <div className="align-middle animated rubberBand delay-0s">
-                  <p>
-                    <b>Objetivos</b>
-                  </p>
-                  <ul>
-                    <li>Tiempos de conversación.</li>
-                    <li>Resolución de incidencias.</li>
-                    <li>Cliente satisfecho.</li>
-                  </ul>
-                </div>
-              </Col>
-              <Col xs="12" sm="12" md="5" lg="4">
-                <img
-                  src={Objetivo}
-                  style={{ width: 360 }}
-                  alt="Objetivo"
-                  className="img-fluid bordeImagen animated rubberBand delay-1s"
-                />
-              </Col>
-            
+            <Col xs="12" sm="12" md="5" lg="5">
+              <div className="align-middle animated rubberBand delay-0s">
+                <p>
+                  En este curso te proporcionaremos las herramientas necesarias
+                  para llevar a cabo tu llamada y logres el modelo de la
+                  etiqueta telefónica al cubrir las necesidades de cada cliente
+                  de tu campaña.
+                </p>
+              </div>
+            </Col>
+            <Col xs="12" sm="12" md="5" lg="4">
+              <img
+                src={Objetivo}
+                style={{ width: 360 }}
+                alt="Objetivo"
+                className="img-fluid bordeImagen animated rubberBand delay-1s"
+              />
+            </Col>
+            <Col xs="12" sm="12" md="5" lg="4">
+              <h3 className="animated rubberBand delay-2s">
+                <b>¡COMENCEMOS!</b>
+              </h3>
+            </Col>
           </Row>
         </CardBody>
       </div>
