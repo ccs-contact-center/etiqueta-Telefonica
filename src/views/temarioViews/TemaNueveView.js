f592cf571b7b4e3cfdaba936c030a0303971d1b7
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-11 16:42:32 -0500

    Create TemaNueveView.js

---
+++ src/views/temarioViews/TemaNueveView.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { CardBody, CardHeader, Col, Row } from 'reactstrap'
+
+import Tabla1 from './componentsExtras/tablas'
+
+
+class TemaSeisView extends Component {
+  loading = () => (
+    <div className="animated fadeIn pt-1 text-center">Cargando...</div>
+  )
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <CardHeader className="text-left">
+          <h3>ETIQUETA TELEFÓNICA</h3>
+        </CardHeader>
+        <CardBody className="">
+          <Row className="centrado-fila">
+            <Col xs="12">
+              <h2>
+                <b>HABILIDADES NECESARIAS</b>
+              </h2>
+            </Col>
+            <Col xs="12">
+              <p>
+                Algunas frases que puedes ocupar para empatizar y conectar con
+                el cliente pueden ser:
+              </p>
+              <Tabla1/>
+            </Col>
+          </Row>
+        </CardBody>
+      </div>
+    )
+  }
+}
+export default TemaSeisView
