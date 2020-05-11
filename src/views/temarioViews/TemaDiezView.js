837d65fa8b7ffb7514bb03700338112ee254f1e7
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-11 17:28:50 -0500

    Create TemaDiezView.js

---
+++ src/views/temarioViews/TemaDiezView.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { CardBody, CardHeader, Col, Row } from 'reactstrap'
+import Elemetos from '../../assets/img/ImgEtiquetaTelefonica/elemetos.png'
+
+class TemaSeisView extends Component {
+  loading = () => (
+    <div className="animated fadeIn pt-1 text-center">Cargando...</div>
+  )
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <CardHeader className="text-left">
+          <h3>ETIQUETA TELEFÓNICA</h3>
+        </CardHeader>
+        <CardBody className="">
+          <Row className="centrado-fila">
+            <Col xs="12">
+              <h2>
+                <b>HABILIDADES TÉCNICAS</b>
+              </h2>
+            </Col>
+            <Col xs="12">
+              <p>
+                La Comunicación es la transmisión de un mensaje de un emisor a
+                un receptor mediante la utilización de un codigo conocido por
+                ambos y con un objetivo determinado.
+              </p>
+            </Col>
+          </Row>
+          <Row className="centrado-fila">
+            <Col xs="12" sm="4" md="3">
+              <img
+                src={Elemetos}
+                style={{ width: 260 }}
+                alt="icono.png"
+                className="img-fluid  "
+              />
+            </Col>
+            <Col xs="12" sm="8" md="9">
+              <p>
+                <b>La voz:</b> elemento fundamental de la comunicación
+                telefónica
+              </p>
+              <p>
+                <b>
+                  Todo Agente utiliza su voz como medio de trabajo y por tanto
+                  la Voz se convierte en un aspecto a cuidar y proteger.
+                </b>
+              </p>
+              <p>
+                La voz, bien utilizada es capaz de expresar y transmitir una
+                infinidad de emociones, sensaciones y matices diferentes. Esto
+                hace el mensaje mas atractivo e impactante para la gente que lo
+                escucha.
+              </p>
+            </Col>
+          </Row>
+        </CardBody>
+      </div>
+    )
+  }
+}
+export default TemaSeisView
