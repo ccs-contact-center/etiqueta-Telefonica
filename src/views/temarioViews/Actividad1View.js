9d3d953f37b9b6dbbcc8f6fa08776e44b9cc4ce1
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-06-02 18:15:49 -0500

    Update Actividad1View.js

--- src/views/temarioViews/Actividad1View.js
+++ src/views/temarioViews/Actividad1View.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { CardBody, CardHeader, Col, Row } from 'reactstrap'
-import SweetAlert from 'sweetalert2-react'
+
 import Actividad1 from './componentsExtras/dragdrog'
 
 class ActividadView extends Component {
