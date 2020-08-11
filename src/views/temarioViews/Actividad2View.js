c38bfdcc875c7e4155aa65adc4d17d8db822ac17
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-08-11 14:59:50 -0500

    Update Actividad2View.js

--- src/views/temarioViews/Actividad2View.js
+++ src/views/temarioViews/Actividad2View.js
@@ -10,7 +10,7 @@ import {
   Label,
   Input,
 } from 'reactstrap'
-import SweetAlert from 'sweetalert2-react'
+import swal from "sweetalert"
 //import { bind } from 'core-js/fn/function'
 import AuthService from "../../services/AuthService";
 import API_CCS from '../../services/API_CCS'
@@ -48,10 +48,37 @@ class ActividadView extends Component {
 
   async onSave(e) {
     try {
-      var respuesta = await API.guardaActividad(this.state)
-      alert('Se guardo actividad: 2, con id ' + respuesta[0].id)
+      var respuesta = await API.guardaActividad(this.state);
+
+      swal({
+        title: "Status Actividad",
+        text: "Se guardo la actividad: 1, con id: " + respuesta[0].id,
+        icon: "success",
+        dangerMode: true,
+        button: {
+          text: "Aceptar",
+          value: true,
+          visible: true,
+          className: "btn btn-primary",
+          reset: true,
+        },
+      });
+      
+      // alert("Se guardo la actividad: 1, con id: " + respuesta[0].id);
     } catch (err) {
-      console.log('loggea si hay un error')
+      swal({
+        title: "Status Actividad",
+        text: "No se guardo la actividad: 1, Intenta de nuevo. ",
+        icon: "error",
+        dangerMode: true,
+        button: {
+          text: "Cerrar",
+          value: true,
+          visible: true,
+          className: "btn btn-primary ",
+        },
+      });
+      console.log("loggea si hay un error");
     }
   }
 
@@ -65,20 +92,12 @@ class ActividadView extends Component {
 
   // }
 
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value)
-    event.preventDefault()
-  }
+ 
 
   render() {
     return (
       <div className="animated fadeIn">
-        <SweetAlert
-          show={this.state.show}
-          title="Enviado"
-          text="Respuestas enviadas...."
-          onConfirm={() => this.setState({ show: false })}
-        />
+        
         <Row>
           <Col>
             <CardHeader className="">
